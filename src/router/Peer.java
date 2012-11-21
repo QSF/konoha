19bf53c387e2b3595559ba8dc2ccd69fb703207e
viniciusffj <viniciusffj@gmail.com>
2012-11-21 17:33:37 -0200

    Comentários no pacote router foram adicionados.

--- src/router/Peer.java
+++ src/router/Peer.java
@@ -1,5 +1,17 @@
 package router;
 
+/**
+ * Objeto que representa um peer.
+ * */
 public class Peer {
-
+	private String ip;
+	
+	public String getIp() {
+		return ip;
+	}
+	
+	public void setIp(String ip) {
+		//formatar.
+		this.ip = ip;
+	}
 }
--- src/router/Router.java
+++ src/router/Router.java
@@ -2,9 +2,33 @@ package router;
 
 import java.util.ArrayList;
 
+/**
+ * Classe responsável pelo roteamento.
+ * */
 public class Router {
 	
+	/**Lista de peers vizinhos*/
 	private ArrayList<Peer> peers;
+	/**Nome do arquivo*/
 	private String fileName;
 	
+	/**
+	 * Método que encontra os peers vizinhos.
+	 * */
+	public void findNeighbor(){
+		
+	}
+	
+	public ArrayList<Peer> getPeers() {
+		return peers;
+	}
+	public void setPeers(ArrayList<Peer> peers) {
+		this.peers = peers;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}	
 }
